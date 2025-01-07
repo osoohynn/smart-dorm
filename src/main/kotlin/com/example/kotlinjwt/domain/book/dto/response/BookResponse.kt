79b2cc1e462e359486de0cc88754a5da6a75dde1
su080915 <tbr335@naver.com>
2025-01-07 16:11:51 +0900

    fix: book response

--- src/main/kotlin/com/example/kotlinjwt/domain/book/dto/response/BookResponse.kt
+++ src/main/kotlin/com/example/kotlinjwt/domain/book/dto/response/BookResponse.kt
@@ -2,13 +2,15 @@ package com.example.kotlinjwt.domain.book.dto.response
 
 import com.example.kotlinjwt.domain.book.domain.entity.Book
 import com.example.kotlinjwt.domain.book.domain.enums.RoomType
+import com.example.kotlinjwt.domain.user.dto.response.UserResponse
 import java.time.LocalDateTime
 
 data class BookResponse(
     val roomType: RoomType,
     val time: LocalDateTime,
     val endTime: LocalDateTime,
-    val number: Int
+    val number: Int,
+    val bookedBy: UserResponse,
 ) {
     companion object {
         fun of(book: Book): BookResponse {
@@ -17,6 +19,7 @@ data class BookResponse(
                 time = book.time,
                 endTime = book.endTime,
                 number = book.number,
+                bookedBy = UserResponse.of(book.bookedBy)
             )
         }
     }
